a95b264c29c820aafb0321b997dd97dc694017be
Devesh <deveshtandel48@gmail.com>
2026-06-01 20:25:19 +0530

    Render deployment fix

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
@@ -56,4 +57,7 @@ def home():
     )
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000))
+    )
